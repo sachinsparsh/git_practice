6974242faabce3985fb83954302723881cb03e61
sachin <sachinsparsh525@gmail.com>
2025-11-12 22:36:39 +0530

    c programs

---
+++ c programs/big3.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+void biggestOf3() {
+    int a, b, c, largest;
+    printf("Enter three numbers: ");
+    scanf("%d %d %d", &a, &b, &c);
+    largest = (a > b) ? ((a > c) ? a : c) : ((b > c) ? b : c);
+    printf("Largest number is: %d\n", largest);
+    printf("\n");
+}
---
+++ c programs/fact.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+void factorial() {
+    int n;
+    unsigned long long factorial = 1;  // Use unsigned long long for large results
+
+    printf("Enter a number: ");
+    scanf("%d", &n);
+
+    if (n < 0)
+        printf("Factorial of a negative number doesn't exist.\n");
+    else {
+        for (int i = 1; i <= n; ++i) {
+            factorial *= i;
+        }
+        printf("Factorial of %d = %llu\n", n, factorial);
+        printf("\n");
+    }
+}
